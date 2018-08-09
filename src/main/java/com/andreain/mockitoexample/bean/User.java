3fc457023465a82f6efb20c3bc45d0d9e31af40a
Ainoa Andres <Ainoa.Andres@adidas.com>
2018-08-09 09:35:40 +0200

    Reformat code

--- src/main/java/com/andreain/mockitoexample/bean/User.java
+++ src/main/java/com/andreain/mockitoexample/bean/User.java
@@ -8,27 +8,29 @@ import java.util.Map;
  */
 public class User {
 
-    private Map<String,String> userRoles;
+    private Map<String, String> userRoles;
 
-    public User(){
+    public User() {
         this.userRoles = new HashMap<String, String>();
     }
 
     /**
      * Put the different roles/users into the User's bean.
+     *
      * @param newRole the new role of the user
-     * @param user the name of the user
+     * @param user    the name of the user
      */
-    public void setRole(String newRole,String user){
-        this.userRoles.put(newRole,user);
+    public void setRole(String newRole, String user) {
+        this.userRoles.put(newRole, user);
     }
 
     /**
      * Put the different roles/users into the User's bean.
+     *
      * @param role the new role of the user
      * @return the user associated with the role
      */
-    public String getRole(String role){
+    public String getRole(String role) {
         return this.userRoles.get(role);
     }
 }
